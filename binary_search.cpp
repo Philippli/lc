1320c08e3615837dff770954dc0dd9d8d011b7bd
Philippli <Philip.p.li@outlook.com>
2016-09-03 15:58:39 -0400

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,27 @@
+int lowerBound(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right) {
+        int mid = ((right - left) >> 1) + left;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+int upperBound(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left <= right) {
+        int mid = ((right - left) >> 1) + left;
+        if (nums[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
